5112f19b8fc1ee52cb60a9d16cb6fcfb97739fc2
Hack Reactor Student <student@hackreactor.com>
2017-09-01 15:22:02 -0700

    Implement fetch. Pass spec.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,6 @@
 // YOUR CODE HERE:
 var app = {
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
   init: function() {
     
   },
@@ -22,7 +23,20 @@ var app = {
     
   },
   fetch: function() {
-    
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });    
   }
 };
 
