c9fce69ddb3c2248a96c595e98f96576c7121233
Hack Reactor Student <student@hackreactor.com>
2017-09-01 15:27:34 -0700

    Implement clearMessages method. Passed spec

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,7 +7,7 @@ var app = {
   send: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      url: this.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -19,13 +19,11 @@ var app = {
         console.error('chatterbox: Failed to send message', data);
       }
     });    
-    
-    
   },
   fetch: function() {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      url: this.server,
       type: 'GET',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -37,6 +35,9 @@ var app = {
         console.error('chatterbox: Failed to send message', data);
       }
     });    
+  },
+  clearMessages: function() {
+    $('#chats').html('');
   }
 };
 
