d75662fded6a354833681e72af8e42e1d9e99ec0
Hack Reactor Student <student@hackreactor.com>
2017-09-01 19:41:48 -0700

    Implement fetch from Parse server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,14 @@
 // YOUR CODE HERE:
 var app = {
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  username: 'Kenny',
+  roomname: 'lobby',
   init: function() {
     console.log('init');
     $('.username').on('click', app.handleUsernameClick);
     $('.submit').on('submit', app.handleSubmit);
+    
+    app.fetch();
   },
   send: function(message) {
     $.ajax({
@@ -26,15 +30,28 @@ var app = {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: app.server,
+      data: 'order=-createdAt&limit=50',
       type: 'GET',
-      data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        if (data.results.length === 0) {
+          return;
+        }
+        for (var i = 0; i < data.results.length; i++) {
+          var message = {
+            username: data.results[i].username,
+            text: data.results[i].text,
+            roomname: data.results[i].roomname,
+            time: data.results[i].createdAt
+          };
+          app.renderMessage(message);
+        }
+        console.log(data.results);
+        console.log('chatterbox: Message received');
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
+        console.error('chatterbox: Failed to receive message', data);
       }
     });    
   },
@@ -47,7 +64,7 @@ var app = {
     //       text: 'I didn\'t get a harumph outa that guy.!',
     //       roomname: 'lobby'
     // }
-    $('#chats').append('<p>' + '<a src="#" class = "username">' + message.username + ':' + message.text + '--' + message.roomname + '</p>');
+    $('#chats').append('<p>' + message.time + '\t\t' + '<a src="#" class = "username">' + message.username + ':' + message.text + '--' + message.roomname + '</p>');
   },
   renderRoom: function(roomName) {
     var $option = $('<option>').val(roomName).text(roomName);
